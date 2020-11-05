4371c22f033cd63f02265a95311f0312e487b1e1
ka3a4ook <89052585288@mail.ru>
2020-11-06 00:45:16 +0300

    Созданы пакеты для разных зон ответственности

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Application
+from model.group import Group
+from fixture.application import Application
 
 
 @pytest.fixture
@@ -17,7 +17,7 @@ def test_add_group(app):
     app.logout()
 
 
-def test_add_empty_group(app):
+"""def test_add_empty_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.logout()"""
