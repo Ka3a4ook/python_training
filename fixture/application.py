997b0c6a67d47e1de1ade27676faaa4bbb16fb06
ka3a4ook <89052585288@mail.ru>
2020-11-06 12:12:18 +0300

    Создан класс-менеджер, который инициализирует всех помощников

--- fixture/application.py
+++ fixture/application.py
@@ -11,21 +11,6 @@ class Application:
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/")
-
-    def destroy(self):
-        self.wd.quit()
-
-
-class ApplicationContact:
-
-    def __init__(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-        self.session = SessionHelper(self)
         self.contact = ContactHelper(self)
 
     def open_home_page(self):
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 import pytest
 from model.contact import Contact
-from fixture.application import ApplicationContact
+from fixture.application import Application
 
 
 @pytest.fixture
 def app(request):
-    fixture = ApplicationContact()
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
