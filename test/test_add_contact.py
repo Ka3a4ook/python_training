5eb0fdb352f5ed6cfe453c2eba2c212a80f3ec64
ka3a4ook <89052585288@mail.ru>
2020-11-09 00:37:08 +0300

    Добавлена валидация фикстуры перед каждым тестом

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,11 +3,9 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Andrey", lastname="Kazakevich", title="Engineer", company="Bercut",
                                home_phone="4226668", mobile_phone="89214226628",
                                email="89214226628@mail.ru", bday="4", bmonth="March", byear="1995",
                                country="Russia",
                                city="Saint-Petersburg",
                                notes="Hello everyone :)"))
-    app.session.logout()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,6 +3,4 @@ from model.contact import Contact
 
 
 def test_modify_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="Fedor", lastname="Emelyanenko"))
-    app.session.logout()
