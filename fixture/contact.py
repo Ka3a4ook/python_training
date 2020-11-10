ad8b544dbbb97b1745ef50f8696b18c24d0ff47c
ka3a4ook <89052585288@mail.ru>
2020-11-10 11:51:18 +0300

    Добавлен переход на главную страницу перед подсчетом количества контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -65,12 +65,13 @@ class ContactHelper:
         self.fill_contact_form(new_contact_data)
         # submit modification
         wd.find_element_by_name("update").click()
-        wd.find_element_by_link_text("home page").click()
+        self.return_to_home_page()
 
     def return_to_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home page").click()
+        wd.find_element_by_link_text("home").click()
 
     def count(self):
         wd = self.app.wd
+        self.return_to_home_page()
         return len(wd.find_elements_by_name("selected[]"))
