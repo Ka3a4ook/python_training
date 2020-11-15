6f26914f5a61b6d62284da8056fdb673e24246cb
ka3a4ook <89052585288@mail.ru>
2020-11-15 11:52:16 +0300

    Добавлено сравнение списка групп до и после прохождения тестов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,8 +3,14 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="abrakadabra", header="abrakadabra", footer="abrakadabra"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 """def test_add_empty_group(app):
-    app.group.create(Group(name="", header="", footer=""))"""
+    old_groups = app.group.get_group_list()
+    app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)"""
