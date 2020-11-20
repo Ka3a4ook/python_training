52f936ece7a44a983e60f06bf603995871bec4b5
ka3a4ook <89052585288@mail.ru>
2020-11-20 13:29:41 +0300

    Доработаны функции modify_first_group и modify_first_contact

--- fixture/group.py
+++ fixture/group.py
@@ -54,7 +54,7 @@ class GroupHelper:
         wd.find_elements_by_name("selected[]")[index].click()
 
     def modify_first_group(self, new_group_data):
-        self.modify_group_by_index(0)
+        self.modify_group_by_index(0, new_group_data)
 
     def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
