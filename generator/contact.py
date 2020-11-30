9aca8c1e491074ac897ec297c9f01360f8405a50
ka3a4ook <89052585288@mail.ru>
2020-11-30 11:38:49 +0300

    Реализована загрузка тестовых данных из файлов в формате JSON и параметризация тестов

--- generator/contact.py
+++ generator/contact.py
@@ -71,4 +71,5 @@ testdata = [Contact(firstname=random_string("firstname", 20), lastname=random_st
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(jsonpickle.encode(testdata, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,11 +1,8 @@
 from model.group import Group
-from data.add_group import testdata
-import pytest
 
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
-    pass
+def test_add_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     new_groups = app.group.get_group_list()
